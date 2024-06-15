980eb31b49d42970f78684a68e019d28d014d1d5
lion <1738376480@qq.com>
2024-06-15 17:12:44 +0800

    first commit

---
+++ build/CMakeFiles/rknn_yolov5_video_demo.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/orangepi/rknpu2/examples/rknn_yolov5_demo/src/main_video.cc" "/home/orangepi/rknpu2/examples/rknn_yolov5_demo/build/CMakeFiles/rknn_yolov5_video_demo.dir/src/main_video.cc.o"
+  "/home/orangepi/rknpu2/examples/rknn_yolov5_demo/src/postprocess.cc" "/home/orangepi/rknpu2/examples/rknn_yolov5_demo/build/CMakeFiles/rknn_yolov5_video_demo.dir/src/postprocess.cc.o"
+  "/home/orangepi/rknpu2/examples/rknn_yolov5_demo/utils/drawing.cpp" "/home/orangepi/rknpu2/examples/rknn_yolov5_demo/build/CMakeFiles/rknn_yolov5_video_demo.dir/utils/drawing.cpp.o"
+  "/home/orangepi/rknpu2/examples/rknn_yolov5_demo/utils/mpp_decoder.cpp" "/home/orangepi/rknpu2/examples/rknn_yolov5_demo/build/CMakeFiles/rknn_yolov5_video_demo.dir/utils/mpp_decoder.cpp.o"
+  "/home/orangepi/rknpu2/examples/rknn_yolov5_demo/utils/mpp_encoder.cpp" "/home/orangepi/rknpu2/examples/rknn_yolov5_demo/build/CMakeFiles/rknn_yolov5_video_demo.dir/utils/mpp_encoder.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BUILD_VIDEO_RTSP"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../../../runtime/RK3588/Linux/librknn_api/include"
+  "../../3rdparty"
+  "../../3rdparty/rga/RK3588/include"
+  "../../3rdparty/mpp/include"
+  "../../3rdparty/zlmediakit/include"
+  "../include"
+  "/home/orangepi/rknpu2/examples/3rdparty/opencv/opencv-linux-aarch64/include"
+  "/home/orangepi/rknpu2/examples/3rdparty/opencv/opencv-linux-aarch64/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
